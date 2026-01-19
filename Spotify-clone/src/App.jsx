0ceaecdcf568da02eef1a029628175d79a9f408a
bhawesh wasnik <bhaweshwasnik4@gmail.com>
2026-01-19 23:30:45 +0530

    router initialization

--- Spotify-clone/src/App.jsx
+++ Spotify-clone/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='bg-black h-screen text-white text-2xl font-bold'>
-      <h1 className='text-center bg-blue-400'>Aksath Lavde Gandu</h1>
-    </div>
+    <Router>
+      <div className='bg-black h-screen text-white text-2xl font-bold'>
+        <h1 className='text-center bg-blue-400'>Aksath Lavde Gandu</h1>
+      </div>
+    </Router>
+
   )
 }
 
