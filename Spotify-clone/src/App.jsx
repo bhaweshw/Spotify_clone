636732425928cd33ff51a70f61abff287357c774
bhawesh wasnik <bhaweshwasnik4@gmail.com>
2026-01-19 22:30:40 +0530

    initial setup

---
+++ Spotify-clone/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen text-white text-2xl font-bold'>
+      <h1 className='text-center'>Aksath Lavde</h1>
+    </div>
+  )
+}
+
+export default App
