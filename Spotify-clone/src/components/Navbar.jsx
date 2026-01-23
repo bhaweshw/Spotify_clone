ec9cd9fd94c6062fefb8135f53ed6193ea26edd2
Sugarkandee <akshatkashyap7771@gmail.com>
2026-01-23 08:53:34 +0530

    white spotify icon fixing

--- Spotify-clone/src/components/Navbar.jsx
+++ Spotify-clone/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import NavIcon from '../assets/Navicon.svg'
+
 
 function Navbar() {
   return (
     <nav className="text-white flex gap-0.5 justify-between">
-      <div>NavIcon</div>
+      <div><img src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_White.png" alt="" /></div>
       <ul className="flex justify-around w-[50vw]">
       <div>spacebar</div>
         <li><button/></li>
