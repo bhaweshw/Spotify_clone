c0c068623910a13355b9d50dbb86c5c1c0a9e976
bhawesh wasnik <bhaweshwasnik4@gmail.com>
2026-01-27 16:14:50 +0530

    home page optimization

--- Spotify-clone/src/components/Player.jsx
+++ Spotify-clone/src/components/Player.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Player = () => {
   return (
-    <div>
+    <div className="bg-black w-full fixed bottom-0">
       <h1 className='text-center bg-red-400'>Player</h1>
     </div>
   )
