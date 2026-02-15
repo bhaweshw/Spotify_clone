e828b8b9708fdc17d3c3dd15775205f3871c797c
bhaweshw <bhaweshwasnik4@gmail.com>
2026-02-15 18:39:45 +0530

    player color update

--- Spotify-clone/src/components/Player.jsx
+++ Spotify-clone/src/components/Player.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Player = () => {
   return (
     <div className="bg-black w-full fixed bottom-0">
-      <h1 className='text-center bg-red-400'>Player</h1>
+      <h1 className='text-center bg-blue-400'>Player</h1>
     </div>
   )
 }
